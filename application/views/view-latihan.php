76a6c53fdf163996cb573e7a53c4672c4bb8231c
Amanda <you@example.com>
2023-04-05 20:55:42 +0700

    first commit

---
+++ application/views/view-latihan.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo Kawan.. Yuk kita belajar web programming..!!<br>
+    Nilai 1 = <?= $n1; ?>
+    Nilai 2 = <?= $n2; ?>
+    Ini hasil dari pemodelan dengan metode penjumlahan yaitu <?=
+$n1 . " + " . $n2 . " = " . $hasil;?>
+
+</body>
+</html>
